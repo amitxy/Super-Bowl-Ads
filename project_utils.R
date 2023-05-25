e49db8251f47b3936cd590bf823d7c00213c5d82
amitxy <amitxy96@gmail.com>
2023-05-25 13:14:15 +0300

    added funcs

--- project_utils.R
+++ project_utils.R
@@ -1,6 +1,22 @@
 library(tidyverse)
 
-test <- function(x,y)
+data <-  read.csv("data\\superbowl-ads.csv")
+rating <-  read.csv("data\\super-bowl-ratings.csv")[-55,] %>% 
+  mutate(year=year(date))
+general <- read.csv("data\\superbowl-ads-general.csv")
+
+#fmt <- "%Y-%m-%d"
+#rating[["date"]] <- as.Date(rating[["date"]],fmt)
+
+search_dates <- function(brand)
 {
-  return(x+y)
-}1
+  brand_history <- data %>% 
+    group_by(brand, year) %>% 
+    filter(brand ==brand_name)
+  
+  r <- rating %>% 
+    filter(year %in% brand_history[["year"]])
+  
+  return(r["date"])
+}
+  
